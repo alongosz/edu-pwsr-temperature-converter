8e30ec7e4c856623e853071b071426fb64ca4f7a
Andrzej Longosz <andrzej@longosz.net>
2017-06-09 22:57:36 +0200

    Improved event handling

--- src/net/longosz/TemperatureConverter/Controller.java
+++ src/net/longosz/TemperatureConverter/Controller.java
@@ -1,11 +1,9 @@
 package net.longosz.TemperatureConverter;
 
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import net.longosz.TemperatureConverter.converters.CelsiusConverter;
 import net.longosz.TemperatureConverter.converters.FahrenheitConverter;
 import net.longosz.TemperatureConverter.converters.IConverter;
@@ -13,10 +11,6 @@ import net.longosz.TemperatureConverter.converters.KelvinConverter;
 import net.longosz.TemperatureConverter.errors.UIValidationException;
 
 public class Controller {
-    @FXML
-    public void initialize() {
-        // Nothing to do
-    }
 
     @FXML
     private RadioButton fromCelsius;
@@ -49,26 +43,48 @@ public class Controller {
     private RadioButton toKelvin;
 
     @FXML
+    private Label statusLabel;
+
+    @FXML
+    public void initialize() {
+        final ChangeListener<Toggle> toggleChanged = (observableValue, oldValue, newValue) -> {
+            resetOutput();
+            if (isReady()) {
+                recalculate();
+            }
+        };
+
+        inputScale.selectedToggleProperty().addListener(toggleChanged);
+        outputScale.selectedToggleProperty().addListener(toggleChanged);
+
+        // listen for dynamic changes of input text field
+        inputField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            resetOutput();
+            // immediately display user an error if he entered sth different than number
+            if (isInputValueValid() && isReady()) {
+                recalculate();
+            }
+        });
+    }
+
+    @FXML
     void inputChanged(ActionEvent event) {
+        resetOutput();
         recalculate();
     }
 
+    private void resetOutput() {
+        outputField.setText("");
+        setStatus("");
+    }
+
     private void recalculate() {
-
-        if (inputField.getText().trim().isEmpty()) {
-            outputField.setText("");
-            return;
-        }
-
         try {
-            float inputValue = Float.parseFloat(inputField.getText().trim());
+            float inputValue = getInputNumber();
             double result = convert(inputValue);
-            outputField.setText(Double.toString(result));
-        } catch (NumberFormatException e) {
-            inputField.requestFocus();
-            showErrorDialog("Provided input value should be a decimal number");
+            outputField.setText(String.format("%.4f", result));
         } catch (UIValidationException e) {
-            System.err.println(e.getMessage());
+            setStatus(e.getLocalizedMessage());
             showErrorDialog(e.getLocalizedMessage());
         }
     }
@@ -90,7 +106,7 @@ public class Controller {
     private IConverter getConverter() throws UIValidationException {
         if (fromCelsius.isSelected()) {
             return new CelsiusConverter();
-        } else  if (fromFahrenheit.isSelected()) {
+        } else if (fromFahrenheit.isSelected()) {
             return new FahrenheitConverter();
         } else if (fromKelvin.isSelected()) {
             return new KelvinConverter();
@@ -105,5 +121,50 @@ public class Controller {
         alert.setHeaderText(message);
 
         alert.showAndWait();
+    }
+
+    /**
+     * Check if all the input data are ready for calculation of converted temperature w/o throwing exception
+     *
+     * @return bool
+     */
+    private boolean isReady() {
+        return !inputField.getText().trim().isEmpty() &&
+                // both scales have to be selected for calculations to make sense
+                inputScale.getSelectedToggle() != null &&
+                outputScale.getSelectedToggle() != null;
+    }
+
+    /**
+     * @return float
+     */
+    private float getInputNumber() throws UIValidationException {
+        try {
+            return Float.parseFloat(inputField.getText().trim());
+        } catch (NumberFormatException e) {
+            inputField.requestFocus();
+            throw new UIValidationException("Provided input value should be a decimal number");
+        }
+    }
+
+    /**
+     * Only check if it is possible to convert input to float.
+     */
+    private boolean isInputValueValid() {
+        if (inputField.getText().isEmpty()) {
+            // empty value is considered valid at this point due to usability, {@see isReady} method
+            return true;
+        }
+        try {
+            getInputNumber();
+            return true;
+        } catch (UIValidationException e) {
+            setStatus(e.getLocalizedMessage());
+            return false;
+        }
+    }
+
+    private void setStatus(String message) {
+        statusLabel.setText(message);
     }
 }
